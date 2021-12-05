71f839a8ed5abdc53bb482e7124d03af4c9e2560
Cagri Bicer <bicer@outlook.com>
2021-12-05 20:59:05 +0300

    2nd Test Commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print("Test Print 1")
+        print("Test Print 2")
     }
 
 
